2915f6cb16581eb0fda07d4298f541224e5a7c39
xndadelin <adelinmihai071@gmail.com>
2025-08-19 16:38:16 +0300

    Did not migrate AddSuperAdminToUsers correctly.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_104719) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_19_133723) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
